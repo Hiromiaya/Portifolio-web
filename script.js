1afc83ec2f47a602f11ec9c6dc174bd576c6e618
Isaac Alessandro Andrade Medeiros <hiromiaya1227@gmail.com>
2025-12-09 10:53:00 -0300

    correcao final

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
     const themeSwitch = document.getElementById('theme-toggle'); 
     
     if (!themeSwitch) {
-        console.error("Elemento com id 'theme-toggle' não encontrado.");
+        console.error('theme-toggle' não encontrado.");
         return; 
     }
     let darkmodeState = localStorage.getItem('darkmode');
     const body = document.body;
 
     const enableDarkmode = () => {
-        body.classList.add('dark-mode');
+        body.classList.add('darkmode');
         localStorage.setItem('darkmode', 'active');
         themeSwitch.textContent = 'Modo Claro'; 
     };
 
     const disableDarkmode = () => {
-        body.classList.remove('dark-mode');
+        body.classList.remove('darkmode');
         localStorage.setItem('darkmode', 'desligado'); 
         themeSwitch.textContent = 'Modo Escuro';
     };
@@ -26,9 +26,9 @@
 
     let temaEstaAtivo = body.classList.contains('dark-mode');
     if (temaEstaAtivo) {
-        console.log("O tema escuro está ligado, sim!");
+        console.log("O tema escuro está ligado.");
     } else {
-        console.log("O tema escuro está desligado. Vamos esperar um clique.");
+        console.log("O tema escuro está desligado.");
     }
     
     themeSwitch.addEventListener('click', function() {
